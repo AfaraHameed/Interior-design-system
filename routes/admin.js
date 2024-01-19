10ea38c5e3cbf0c1520f33d30708450dd5ab88af
AfaraHameed <afarasidheek@gmail.com>
2024-01-19 21:23:27 +0530

    updated verification

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,11 @@ const adminController = require('../controller/admin');
 const { verifyTokenHandler,verifyRole } = require("../middlewares/jwtHandler");
 router.post('/user',[verifyTokenHandler,verifyRole('admin')],adminController.createUserByAdmin)
 router.delete('/user/:id',[verifyTokenHandler,verifyRole('admin')],adminController.deletUserAccount)
-router.route('/project',[verifyRole('admin')]).post(adminController.addProject).get(adminController.getProjects,)
-//router.post('/project',adminController.addProject)
+//router.route('/project',[verifyRole('admin')]).post(adminController.addProject).get(adminController.getProjects,)
+router.post('/project',[verifyTokenHandler,verifyRole('admin')],adminController.addProject)
+router.get('/project',[verifyTokenHandler,verifyRole('admin')],adminController.getProjects)
 router.post('/project/assign_designer',[verifyTokenHandler,verifyRole('admin')],adminController.assignDesignerToProject)
 router.get('/user',[verifyTokenHandler,verifyRole('admin')],adminController.getAllUsers)
 router.get('/dashboard',[verifyTokenHandler,verifyRole('admin')],adminController.getAdminDashboardDetails)
 router.post('/faq',[verifyTokenHandler,verifyRole('admin')],adminController.addFAQ)
-module.exports=router
+module.exports=router 
